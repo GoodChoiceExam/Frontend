770601a3220ffce70dee92d62caf91314fee936a
Rasmus Møller Kristensen <eaa24ramk@students.eaaa.dk>
2026-05-07 13:25:15 +0200

    Tilføjet JWT auth header til ClassesService

--- FitLife.Frontend/Services/ClassesService.cs
+++ FitLife.Frontend/Services/ClassesService.cs
@@ -1,12 +1,21 @@
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using FitLife.Frontend.Models;
 
 namespace FitLife.Frontend.Services;
 
-public class ClassesService(HttpClient http)
+public class ClassesService(HttpClient http, AuthService authService)
 {
+    private async Task SetAuthHeader()
+    {
+        var token = await authService.GetTokenAsync();
+        if (token is not null)
+            http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+    }
+
     public async Task<List<TrainingClass>> GetAllAsync()
     {
+        await SetAuthHeader();
         return await http.GetFromJsonAsync<List<TrainingClass>>("api/classes") ?? [];
     }
 }
