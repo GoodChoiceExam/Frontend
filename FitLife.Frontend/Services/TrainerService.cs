a6da6d06609e69883cd835085168c1af4d68ced8
Rasmus Møller Kristensen <eaa24ramk@students.eaaa.dk>
2026-05-08 09:54:47 +0200

    Tilføj JWT auth header til TrainerService

--- FitLife.Frontend/Services/TrainerService.cs
+++ FitLife.Frontend/Services/TrainerService.cs
@@ -1,12 +1,21 @@
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using FitLife.Frontend.Models;
 
 namespace FitLife.Frontend.Services;
 
-public class TrainerService(HttpClient http)
+public class TrainerService(HttpClient http, AuthService authService)
 {
+    private async Task SetAuthHeader()
+    {
+        var token = await authService.GetTokenAsync();
+        if (token is not null)
+            http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+    }
+
     public async Task<List<PersonalTrainer>> GetAllAsync()
     {
+        await SetAuthHeader();
         return await http.GetFromJsonAsync<List<PersonalTrainer>>("api/trainers") ?? [];
     }
 }
